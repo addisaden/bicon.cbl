2dcf06f310b01cc3d48509d5bae430efa1872920
Adrian Enns <adria.aden@gmail.com>
2024-04-07 12:07:14 +0200

    run through the bible

--- src/sqliteToDatafile.cbl
+++ src/sqliteToDatafile.cbl
@@ -3,11 +3,23 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+           SELECT BIBLE-TEXT-FILE ASSIGN TO WS-TRANSLATION-NAME
+             ORGANIZATION IS LINE SEQUENTIAL
+             ACCESS MODE IS SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
+       FD BIBLE-TEXT-FILE.
+       01 BIBLE-TEXT-RECORD         PIC X(777).
        WORKING-STORAGE SECTION.
-       01 WS-TRANSLATION-NAME PIC X(250).
-       01 WS-COMMAND          PIC X(250).
+       01 WS-TRANSLATION-NAME       PIC X(250).
+       01 WS-COMMAND                PIC X(250).
+       01 WS-BIBLE-TEXT-EOF         PIC X VALUE "N".
+       01 WS-BIBLE-TEXT-LANGUAGE    PIC X(50).
+       01 WS-BIBLE-TEXT-TRANSLATION PIC X(10).
+       01 WS-BIBLE-TEXT-BOOK        PIC 9(3).
+       01 WS-BIBLE-TEXT-CHAPTER     PIC 9(3).
+       01 WS-BIBLE-TEXT-VERSE       PIC 9(3).
+       01 WS-BIBLE-TEXT-TEXT        PIC X(500).
        LINKAGE SECTION.
        01 SQLITE-DB-FILENAME PIC X(250).
        01 SQLITE-DB-FILEPATH PIC X(250).
@@ -28,5 +40,30 @@
                 " > ",
                 WS-TRANSLATION-NAME
            )
+           PERFORM RUNBIBLETEXTFILE
            EXIT PROGRAM.
        SQLITETODATAFILE-EXIT.
+
+       RUNBIBLETEXTFILE.
+           MOVE "N" TO WS-BIBLE-TEXT-EOF
+           OPEN INPUT BIBLE-TEXT-FILE
+           PERFORM UNTIL WS-BIBLE-TEXT-EOF = "Y"
+               READ BIBLE-TEXT-FILE
+                   AT END
+                       MOVE "Y" TO WS-BIBLE-TEXT-EOF
+                   NOT AT END
+                       UNSTRING BIBLE-TEXT-RECORD
+                           DELIMITED BY "###" INTO
+                             WS-BIBLE-TEXT-LANGUAGE,
+                             WS-BIBLE-TEXT-TRANSLATION,
+                             WS-BIBLE-TEXT-BOOK,
+                             WS-BIBLE-TEXT-CHAPTER,
+                             WS-BIBLE-TEXT-VERSE,
+                             WS-BIBLE-TEXT-TEXT
+                       DISPLAY WS-BIBLE-TEXT-BOOK
+                       DISPLAY WS-BIBLE-TEXT-CHAPTER
+                       DISPLAY WS-BIBLE-TEXT-VERSE
+           END-PERFORM
+           CLOSE BIBLE-TEXT-FILE
+           CONTINUE.
+       RUNBIBLETEXTFILE-EXIT.
