3f82cbec34199ce248479c457d67d84cadaecfe0
Adrian Enns <adria.aden@gmail.com>
2024-05-01 06:32:55 +0200

    list only the file name

--- src/listTranslations.cbl
+++ src/listTranslations.cbl
@@ -6,20 +6,40 @@
            SELECT MetaFile ASSIGN TO WS-META-FILE
                ORGANIZATION IS LINE SEQUENTIAL
                ACCESS MODE IS SEQUENTIAL.
+           SELECT MetaList ASSIGN TO "bibles.meta"
+               ORGANIZATION IS LINE SEQUENTIAL
+               ACCESS MODE IS SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
        FD MetaFile.
        01 MetaFileRecord PIC X(777).
+       01 META-DATA-EOF  PIC X VALUE 'N'.
+       FD MetaList.
+       01 MetaListRecord PIC X(777).
+       01 LIST-DATA-EOF  PIC X VALUE 'N'.
        WORKING-STORAGE SECTION.
-       01 WS-META-FILE PIC X(100).
+       01 WS-META-FILE    PIC X(100).
        LINKAGE SECTION.
        PROCEDURE DIVISION.
-       SEARCH-META-FILES.
-      * https://stackoverflow.com/questions/35879865/how-to-get-all-files-in-directory-in-cobol
       *
       * Search for all files *.bible.meta
       * list the code, language and title
       *
            DISPLAY "Searching for bibles (Not implemented)"
+
+           CALL "system"
+               USING "ls *.bible.meta > bibles.meta"
+           END-CALL
+
+           OPEN INPUT MetaList
+           PERFORM UNTIL LIST-DATA-EOF = 'Y'
+               READ MetaList
+                   AT END
+                       MOVE 'Y' TO LIST-DATA-EOF
+                   NOT AT END
+                       DISPLAY FUNCTION trim(MetaListRecord)
+               END-READ
+           END-PERFORM           
+           CLOSE MetaList
+
            EXIT PROGRAM.
-       SEARCH-META-FILES-EXIT.
